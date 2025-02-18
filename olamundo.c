f998a6cbcf714b1c8f80d4b064f4bbd1b6db8f5e
2di-crypto <dirobapulga@gmail.com>
2025-02-18 11:31:14 +0000

    dadod

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,35 @@
 
+#include <stdio.h>
+
+int main(){
+    float tempertaura, umidade;
+    unsigned int estoque, estoqueminimo = 1000;
+
+    printf("entre com a temperatura: \n");
+    scanf("%f", &tempertaura);
+    printf("entre com umidade: \n");
+    scanf("%f", &umidade);
+    printf("entre com estoque: \n");
+    scanf("%u", &estoque);
+
+
+    if(tempertaura > 30){
+        printf("temperatura esta alta\n");
+    } else {
+        printf("temperatura esta dentro dos parametros\n");
+    }
+
+
+    if(umidade > 50){
+        printf("umidade elevada\n");
+    } else{
+        printf("umidade dentro dos parametros\n");
+    }
+
+    if(estoque < estoqueminimo){
+        printf("estoque abaixo do minimo\n");
+    } else{
+        printf("estoque normal!\n");
+    }
+
+}  
