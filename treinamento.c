39af3ee4a12c4d4105c8a066715779b299382834
2di-crypto <dirobapulga@gmail.com>
2025-03-25 10:25:55 +0000

    ENTRADA DE DADOS

--- treinamento.c
+++ treinamento.c
@@ -18,8 +18,9 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
 //estrutura das cartas
-            printf("=======JOGO DE CARTA SUPER TRUNFO======: \n");
-            printf("=====CARTA 1=====: \n");
+            printf("=======JOGO DE CARTA SUPER TRUNFO====== \n");
+
+    
 // DADOS CARTA 1
             
         char estado_carta1[6];                // Variável para o estado (1 caractere)
@@ -36,7 +37,6 @@ int main() {
         
             printf("\n");
 
-            printf("=====CARTA 2=====: \n");
 
             //dados carta2
         char estado_carta2[6];                // Variável para o estado (1 caractere)
@@ -52,6 +52,8 @@ int main() {
            float pib_per_capita_carta2 ;          // PIB per capita (milhões de reais por habitante)
 
             // Entrada de dados carta1
+            printf("=====CARTA 1===== \n");
+
             printf("ESTADO (Exemplo: SP): \n");
             scanf("%s", estado_carta1);
             
@@ -77,33 +79,11 @@ int main() {
             densidade_populacional_carta1 = (float)populacao_carta1 / area_carta1; // Garantir que a divisão seja feita com float
 pib_per_capita_carta1 = pib_carta1 / populacao_carta1; // PIB per capita em milhões de reais por habitante
         
- // Entrada de dados carta1
-            printf("ESTADO (Exemplo: SP): \n");
-            scanf("%s", estado_carta1);
-            
-            printf("Codigo da cidade (EX: A01,A02,B01,B02): \n");
-            scanf("%s", codigo_carta1); 
-        
-            printf("Nome da cidade: \n");
-            scanf(" %49[^\n]", nome_carta1);  // Captura a linha inteira, permitindo espaços no nome
-        
-            printf("Populacao (em habitantes): \n");
-            scanf("%d", &populacao_carta1);
-        
-            printf("Area (em km²): \n");
-            scanf("%f", &area_carta1);
-        
-            printf("PIB (em milhões de reais): \n");
-            scanf("%f", &pib_carta1);
-        
-            printf("Numero de pontos turísticos: \n");
-            scanf("%d", &numerosdepontosturisticos_carta1);
-        
-            // Cálculos para densidade populacional e PIB per capita
-            densidade_populacional_carta1 = (float)populacao_carta1 / area_carta1; // Garantir que a divisão seja feita com float
-pib_per_capita_carta1 = pib_carta1 / populacao_carta1; // PIB per capita em milhões de reais por habitante
+            printf("\n");
 
              // Entrada de dados carta2
+             printf("=====CARTA 2===== \n");
+
              printf("ESTADO (Exemplo: SP): \n");
              scanf("%s", estado_carta2);
              
@@ -130,7 +110,7 @@ pib_per_capita_carta1 = pib_carta1 / populacao_carta1; // PIB per capita em milh
              pib_per_capita_carta2 = pib_carta2 / populacao_carta2; // PIB per capita em milhões de reais por habitante
 
             // Saída de dados carta1
-            printf("\n===== DADOS DA CIDADE =====\n");
+            printf("\n===== DADOS DA CARTA 1 =====\n");
             printf("Estado: %s\n", estado_carta1);
             printf("Codigo da cidade: %s\n", codigo_carta1);
             printf("Nome da cidade: %s\n", nome_carta1);
@@ -142,7 +122,7 @@ pib_per_capita_carta1 = pib_carta1 / populacao_carta1; // PIB per capita em milh
             printf("PIB per capita: %.2f milhões de reais\n", pib_per_capita_carta1);
             
             // Saída de dados carta2
-            printf("\n===== DADOS DA CIDADE =====\n");
+            printf("\n===== DADOS DA CARTA 2 =====\n");
             printf("Estado: %s\n", estado_carta2);
             printf("Codigo da cidade: %s\n", codigo_carta2);
             printf("Nome da cidade: %s\n", nome_carta2);
