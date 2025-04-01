9b4f2d3b9466aec6bb339e021d67b3d49fe82fbe
2di-crypto <dirobapulga@gmail.com>
2025-04-01 11:50:34 +0000

    dados

--- xadres.c
+++ xadres.c
@@ -4,5 +4,8 @@ printf("**** TORRE ****\n");
 for (int i = 0; i < 5; i++) {
     printf("DIREITA\n");// imprime a direçao do movimento
 }
+printf("\n");
+// mover o bispo 5 casas na diagonal (cima direita) usando loop while
+printf("**** BISPO ****\n");
 return 0;
 }
