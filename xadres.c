45a157df045d7b029548d915ba4e36ad77d0e61f
2di-crypto <dirobapulga@gmail.com>
2025-04-01 11:44:37 +0000

    entrada de dados

---
+++ xadres.c
@@ -0,0 +1,8 @@
+int main(){
+// mover a torre 5 casas a direita usando loop for
+printf("**** TORRE ****\n");
+for (int i = 0; i < 5; i++) {
+    printf("DIREITA\n");// imprime a direçao do movimento
+}
+return 0;
+}
